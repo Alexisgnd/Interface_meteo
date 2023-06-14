9fc4643187f4da72948b4e65cf513962533d91c8
Alexis GONNEAUD <alexis.gonneaud@hotmail.fr>
2023-06-14 09:42:56 +0200

    Commit 3 : Appel API + bouton recherche + gestion erreur

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ const currentWeatherDiv = document.querySelector(".meteo-actuelle"); // Div pour
 const daysForecastDiv = document.querySelector(".days-forecast"); // Div pour afficher les prévisions sur 5 jours
 
 const API_KEY = "0748bcc8f3e9acf712322438b43cfd41"; // Clé API OpenWeatherMap
+
+// Fonction pour obtenir les coordonnées de la ville saisie par l'utilisateur
+const getCityCoordinates = () => {
+  const cityName = cityInput.value.trim();
+  if (cityName === "") return;
+  const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+  // Appel à l'API OpenWeatherMap pour récupérer les coordonnées de la ville
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
+      if (!data.length) return alert(`Aucune coordonnée trouvée pour ${cityName}`);
+      const { lat, lon, name } = data[0];
+      getWeatherDetails(name, lat, lon);
+    })
+    .catch(() => {
+      alert("Une erreur s'est produite lors de la récupération des coordonnées !");
+    });
+}
+
+// Événement du bouton de recherche
+searchButton.addEventListener("click", () => getCityCoordinates());
