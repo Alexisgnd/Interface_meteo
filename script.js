5535a6ee15dbcc01df5cefa20d1a3603d9ce89b9
Alexis GONNEAUD <alexis.gonneaud@hotmail.fr>
2023-06-14 09:33:07 +0200

    Commit 2 : Définition des constantes d'actions

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Sélection des éléments HTML
+const cityInput = document.querySelector("#city-input"); // Champ de saisie pour la ville
+const searchButton = document.querySelector("#search-btn"); // Bouton de recherche
+const currentWeatherDiv = document.querySelector(".meteo-actuelle"); // Div pour afficher les données météo actuelles
+const daysForecastDiv = document.querySelector(".days-forecast"); // Div pour afficher les prévisions sur 5 jours
+
 const API_KEY = "0748bcc8f3e9acf712322438b43cfd41"; // Clé API OpenWeatherMap
